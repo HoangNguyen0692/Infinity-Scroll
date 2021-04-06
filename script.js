0abbd62908f54e896413a4903e458c9cf5d1120e
vng <nguyenvohoang20061992@gmail.com>
2021-04-06 15:11:46 +0200

    add url with api key to get images

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//console.log('Testing'); // browser's windows
+// Unsplash API
+const mCount = 10;
+const mApiKey = 'CmYv-esLCAMLm2N3dBVQvWQ8-0zT80jmF1l90Exl160';
+const mApiUrl = `https://api.unsplash.com/photos/random/?client_id=${mApiKey}&count=${mCount}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const vResponse = await fetch(mApiUrl);
+        const vData     = await vResponse.json();
+    } catch(aError) {   
+        // Catch error
+    }
+}
+
+// On load
+getPhotos();
